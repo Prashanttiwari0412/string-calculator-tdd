e04ea248b1556f6368962a11852a15893a71af07
Prashanttiwari0412 <54036726+Prashanttiwari0412@users.noreply.github.com>
2025-01-13 23:56:38 +0530

    Feat: Implement add method to handle single number and handle multiple numbers in add method

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,4 +1,4 @@
-# **************The method add is implemented to handle a basic case where the input is either an empty string or a string containing up to two comma-separated numbers**************
+#**************The method add is implemented to handle a basic case where the input is either an empty string or a string containing up to two comma-separated numbers**************
 
 # Define the StringCalculator class to implement the `add` method that already support multiple numbers and empty string.
 class StringCalculator:
